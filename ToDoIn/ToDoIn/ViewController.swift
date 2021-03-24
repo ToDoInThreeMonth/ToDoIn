c5994065431857d5aa11e79fa6e1aa7585360a8a
Tatarinova Daria <tatarinovada@student.bmstu.ru>
2021-03-24 21:46:22 +0300

    Создание проекта

---
+++ ToDoIn/ToDoIn/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoIn
+//
+//  Created by Дарья on 24.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
