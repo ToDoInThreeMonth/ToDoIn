503a6484230512936b80987370886faf662f3201
Philya Gurin <gurinfilipp@gmail.com>
2021-03-24 23:24:46 +0300

    vova che za xuynya

--- ToDoIn/ToDoIn/ViewController.swift
+++ ToDoIn/ToDoIn/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Vova che za xuynya")
         // Do any additional setup after loading the view.
     }
 
