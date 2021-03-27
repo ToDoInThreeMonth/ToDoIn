c4488bcaaeeef659d5e2b42c94009a0a59f12969
Philya Gurin <gurinfilipp@gmail.com>
2021-03-27 17:45:24 +0300

    navi bar buttons customized

--- ToDoIn/ToDoIn/ViewController.swift
+++ ToDoIn/ToDoIn/ViewController.swift
@@ -12,9 +12,49 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Vova che za xuynya")
+        
+        setupNavigationBar()
+        
         // Do any additional setup after loading the view.
     }
+    
+    
+    
+    func setupNavigationBar() {
+     
+        navigationItem.title = "ToDoIn"
+        
+        let backNaviBarImage = UIImage(named: "Vector")
+        self.navigationController?.navigationBar.setBackgroundImage(backNaviBarImage, for: .default)
+        
+        let groupButtonImage = UIImage(named: "groupButton")
+        let renderedGroupImage = groupButtonImage?.withRenderingMode(.alwaysOriginal)
+        let groupButton = UIButton(type: .system)
+        
+        groupButton.heightAnchor.constraint(equalToConstant: 45).isActive = true
+        groupButton.widthAnchor.constraint(equalToConstant: 45).isActive = true
+        groupButton.setImage(renderedGroupImage, for: .normal)
+   // groupButton.frame = CGRect(x: 0, y: 0, width: 500, height: 200)
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: groupButton)
+        
+       
+        
+        let addSectionImage = UIImage(named: "addSection")
+        let renderedAddSectionImage = addSectionImage?.withRenderingMode(.alwaysOriginal)
+        let addSectionButton = UIButton(type: .system)
+        addSectionButton.heightAnchor.constraint(equalToConstant: 45).isActive = true
+        addSectionButton.widthAnchor.constraint(equalToConstant: 45).isActive = true
+        addSectionButton.setImage(renderedAddSectionImage, for: .normal)
+   //   addSectionButton.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: addSectionButton)
+        
 
+
+    }
+    
+    
+    
+    
 
 }
 
