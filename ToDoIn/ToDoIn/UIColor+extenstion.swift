85530e1bf16f9c2b8a8ac79a976527250ee6b4e5
Tatarinova Daria <tatarinovada@student.bmstu.ru>
2021-04-03 15:50:55 +0300

    Изменение цветов

--- ToDoIn/ToDoIn/UIColor+extenstion.swift
+++ ToDoIn/ToDoIn/UIColor+extenstion.swift
@@ -1,7 +1,9 @@
 import UIKit
 
 extension UIColor {
-    static let tabBarColor = UIColor(red: 0.953, green: 0.969, blue: 0.98, alpha: 1)
+    static let darkAccentColor = UIColor(red: 0.953, green: 0.969, blue: 0.98, alpha: 1)
+    
+    static let lightAccentColor = UIColor(red: 0.953, green: 0.969, blue: 0.98, alpha: 0.5)
     
     static let darkTextColor = UIColor(red: 0.008, green: 0.173, blue: 0.447, alpha: 1)
     
