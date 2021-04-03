79239629c1ba949eef53769c40e59f5aeaac25c7
Tatarinova Daria <tatarinovada@student.bmstu.ru>
2021-04-03 15:25:42 +0300

    Добавление закруглений на таббар

--- ToDoIn/ToDoIn/Views/CustomTabBar.swift
+++ ToDoIn/ToDoIn/Views/CustomTabBar.swift
@@ -12,7 +12,7 @@ class CustomTabBar: UITabBar {
     private var shapeLayer: CALayer?
     
 //    override func sizeThatFits(_ size: CGSize) -> CGSize {
-//        return CGSize(width: size.width, height: size.height + 55)
+//        return CGSize(width: size.width, height: size.height + 50)
 //    }
     
     private func addShape(_ rect: CGRect) {
@@ -23,9 +23,9 @@ class CustomTabBar: UITabBar {
         shapeLayer.lineWidth = 1.0
         
         shapeLayer.shadowOffset = CGSize(width: 0, height: 0)
-        shapeLayer.shadowRadius = 10
+        shapeLayer.shadowRadius = 7
         shapeLayer.fillColor = UIColor.darkAccentColor.cgColor
-        shapeLayer.shadowOpacity = 0.3
+        shapeLayer.shadowOpacity = 0.2
         
         if let oldShapeLayer = self.shapeLayer {
             self.layer.replaceSublayer(oldShapeLayer, with: shapeLayer)
@@ -44,18 +44,21 @@ class CustomTabBar: UITabBar {
     func createNewPath(_ rect: CGRect) -> CGPath {
         let path = UIBezierPath()
         let screenCenter = rect.width / 2
-        let radius: CGFloat = 40
+        let mainRadius: CGFloat = 40
+        let radius: CGFloat = 20
         let angle: CGFloat = CGFloat.pi * 10 / 180 // угол 10 градусов
-        let yArcCenter = cos(CGFloat.pi / 2 - angle) * radius
-        let minRadius = sin(CGFloat.pi / 2 - angle) * radius
+        let yArcCenter = cos(CGFloat.pi / 2 - angle) * mainRadius
+        let minRadius = sin(CGFloat.pi / 2 - angle) * mainRadius
         let startAngle: CGFloat = -angle
         let endAngle: CGFloat = -CGFloat.pi + angle
         
-        path.move(to: CGPoint(x: 0, y: 0))
+        path.move(to: CGPoint(x: 0, y: radius))
+        path.addArc(withCenter: CGPoint(x: radius, y: radius), radius: radius, startAngle: -CGFloat.pi, endAngle: -CGFloat.pi / 2, clockwise: true)
         path.addLine(to: CGPoint(x: screenCenter - minRadius, y: 0))
-        path.addArc(withCenter: CGPoint(x: screenCenter, y: yArcCenter), radius: radius, startAngle: endAngle, endAngle: startAngle, clockwise: true)
+        path.addArc(withCenter: CGPoint(x: screenCenter, y: yArcCenter), radius: mainRadius, startAngle: endAngle, endAngle: startAngle, clockwise: true)
         path.addLine(to: CGPoint(x: screenCenter + minRadius, y: 0))
-        path.addLine(to: CGPoint(x: self.frame.width, y: 0))
+        path.addLine(to: CGPoint(x: self.frame.width - radius, y: 0))
+        path.addArc(withCenter: CGPoint(x: self.frame.width - radius, y: radius), radius: radius, startAngle: -CGFloat.pi / 2, endAngle: .zero, clockwise: true)
         path.addLine(to: CGPoint(x: self.frame.width, y: self.frame.height))
         path.addLine(to: CGPoint(x: 0, y: self.frame.height))
         path.close()
