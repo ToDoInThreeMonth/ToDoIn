f0fcbc774d947db8aab62b4cc2d40ff032cb2de6
Kamnev Vladimir <camnev.vov@yandex.ru>
2021-04-04 16:14:18 +0300

    Добавил таблицу

--- ToDoIn/ToDoIn/Controllers/ViewController.swift
+++ ToDoIn/ToDoIn/Controllers/ViewController.swift
@@ -6,15 +6,81 @@
 //
 
 import UIKit
+import PinLayout
 
 class ViewController: UIViewController {
     weak var coordinator: MainChildCoordinator?
+    
+    private lazy var offlineTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.backgroundColor = .clear
+        tableView.separatorStyle = .none
+        tableView.allowsSelection = false
+        tableView.register(MainOfflineTableViewCell.self, forCellReuseIdentifier: String(describing: MainOfflineTableViewCell.self))
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(red: 243, green: 247, blue: 250, alpha: 1)
+        setupViews()
     }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        setupLayouts()
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        setupShadows()
+    }
+    
+    private func setupViews() {
+        view.addSubviews(offlineTableView)
+    }
+    
+    private func setupLayouts() {
+        offlineTableView.pin
+            .top(view.pin.safeArea.top)
+            .bottom(view.pin.safeArea.bottom)
+            .horizontally(50)
+            .marginVertical(30)
+    
+    }
+    
+    private func setupShadows() {
+        
+    }
+}
 
+extension ViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return 2
+        case 1:
+            return 2
+        default:
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: MainOfflineTableViewCell.self), for: indexPath) as! MainOfflineTableViewCell
+        cell.textLabel?.text = "123"
+        return cell
+    }
+    
+    
+}
 
+extension ViewController: UITableViewDelegate {
+    
 }
 
