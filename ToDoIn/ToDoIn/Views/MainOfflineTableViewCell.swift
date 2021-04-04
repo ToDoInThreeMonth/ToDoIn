70971dbfe79a52ec25ed1edd0f1e60fc30de78af
Kamnev Vladimir <camnev.vov@yandex.ru>
2021-04-04 15:51:09 +0300

    Создал MainOfflineTableViewCell

---
+++ ToDoIn/ToDoIn/Views/MainOfflineTableViewCell.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class MainOfflineTableViewCell: UITableViewCell {
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("MainOfflineTableViewCell вызвал init?(coder:)")
+    }
+}
